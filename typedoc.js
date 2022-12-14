6e68d0f56df0cc6932385102bde527777dedb9d1
Georgiy Bukharov <souljorje@gmail.com>
2022-12-14 21:08:49 +0400

    chore(deps): update typedoc plugins

--- typedoc.js
+++ typedoc.js
@@ -1,27 +1,34 @@
 module.exports = {
-    out: 'build/typedoc',
-    entryPoints: [
-      'packages/sdk-api',
-      'packages/sdk-events',
-      'packages/ext-taquito'
-    ],
-    entryPointStrategy: 'packages',
-    pluginPages: {
-      pages: [
-        { title: 'VIRTUAL', childrenDir: '../', children: [
-          { title: 'Changelog', source: 'CHANGELOG.md' },
-        ] },
-      ]
-    },
-    exclude: [
-      '**/*.spec.ts',
-      '**/data/**',
-      '**/dist/**',
-      '**/node_modules/**',
-      '**/rollup*.ts',
-      '**/test/**',
-      'examples/**'
-    ],
-    name: 'TzKT SDK',
-    excludePrivate: true,
-  };
+  out: 'build/typedoc',
+  entryPoints: [
+    'packages/sdk-api',
+    'packages/sdk-events',
+    'packages/ext-taquito'
+  ],
+  entryPointStrategy: 'packages',
+  pluginPages: {
+    pages: [
+      { 
+        title: 'VIRTUAL', 
+        childrenDir: '../', 
+        children: [
+          { 
+            title: 'Changelog', 
+            source: 'CHANGELOG.md' 
+          },
+        ] 
+      },
+    ]
+  },
+  exclude: [
+    '**/*.spec.ts',
+    '**/data/**',
+    '**/dist/**',
+    '**/node_modules/**',
+    '**/rollup*.ts',
+    '**/test/**',
+    'examples/**'
+  ],
+  name: 'TzKT SDK',
+  excludePrivate: true,
+};
